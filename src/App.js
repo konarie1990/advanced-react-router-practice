d407c0c9c774f9165cb831f9e43e6c11430d42a5
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-04-02 00:49:56 -0500

    getting Router up and displaying

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from "react";
 import TopNav from "./components/TopNav";
 import SideNav from "./components/SideNav";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Charts from "./components/Charts";
 
 function App() {
   return (
-    <div> 
-      <div id="wrapper">
-        <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
-          <TopNav />
-          <SideNav />
-        </nav>
-        <div style={{backgroundColor: "white"}}>
-          {/* PUT YOUR ROUTES HERE */}
+    <BrowserRouter>
+      <div>
+        <div id="wrapper">
+          <nav
+            className="navbar navbar-inverse navbar-fixed-top"
+            role="navigation"
+          >
+            <TopNav />
+            <SideNav />
+          </nav>
+          <div style={{ backgroundColor: "white" }}>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/charts" component={Charts} />
+          </div>
         </div>
       </div>
-    </div>
-
+    </BrowserRouter>
   );
 }
-
 
 export default App;
